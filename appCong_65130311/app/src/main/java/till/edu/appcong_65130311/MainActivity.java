b8935f0bb43d56d95bc9371fa39450b4385becbd
TrDaiChi <trandaichi0335@gmail.com>
2026-03-15 11:18:40 +0700

    BaiTH2_2: HoanThienAppCong

--- appCong_65130311/app/src/main/java/till/edu/appcong_65130311/MainActivity.java
+++ appCong_65130311/app/src/main/java/till/edu/appcong_65130311/MainActivity.java
@@ -1,6 +1,8 @@
 package till.edu.appcong_65130311;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.edtA), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    //bộ lắng nghe và xử lý
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        //lấy dữ liệu ve` ở điều khiển số A
+        String strA = editTextSoA.getText().toString();
+        //lấy dữ liệu ve` ở điều khiển số B
+        String strB = editTextSoB.getText().toString();
+        //chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        //tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong); //chuyển sang dạng chuỗi
+        //hiện ra màn hình
+        editTextKetQua.setText(strTong);
     }
 }
+
